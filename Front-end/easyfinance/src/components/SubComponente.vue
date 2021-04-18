a050ee398d7eadc9bc09f1ff1e5969ea2be270e9
luizrls <luiz.rogerio.lima@hotmail.com>
2021-04-17 21:41:56 -0300

    no message

---
+++ Front-end/easyfinance/src/components/SubComponente.vue
@@ -0,0 +1,20 @@
+<template>
+<div class='SubComponente'>
+  <ul>
+      <li v-for="(item,index) in items" :key="index">{{item}}</li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+name: 'SubComponente',
+props:{
+    items: Array
+}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Front-end/easyfinance/src/components/TesteComponente1.vue
@@ -0,0 +1,32 @@
+<template>
+<div class='TesteComponente1'>
+  <h3>{{titulo}}</h3>
+  <SubComponente :items="items"/>
+</div>
+</template>
+
+<script>
+import SubComponente from './SubComponente.vue'
+export default {
+name: 'TesteComponente1',
+components:{
+    SubComponente
+},
+props:{
+    titulo: String
+},
+data: ()=>{
+    return {
+        items: ["Novo item1", "Novo Item2", "Novo Item3", "Novo Item3", "Novo Item3", "Novo Item3", "Novo Item3", "Novo Item3"]
+    }
+}
+}
+
+</script>
+
+<style scoped>
+.TesteComponente1{
+    background: black;
+    color: brown;
+}
+</style>
